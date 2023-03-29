1113696557921228ff5f5d2a020b75bd4a0e57d0
long <1831622781@qq.com>
2023-03-29 22:45:01 +0800

    Initial commit

---
+++ src/main/java/com/example/reggie/ReggieTakeOutApplication.java
@@ -0,0 +1,15 @@
+package com.example.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ReggieTakeOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieTakeOutApplication.class, args);
+    }
+
+}
