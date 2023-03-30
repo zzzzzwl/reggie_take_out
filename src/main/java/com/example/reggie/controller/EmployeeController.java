6ae3afff3b7030ab3d49db1a99638ec29ba45952
zwl <1831622781@qq.com>
2023-03-30 17:14:58 +0800

    03301714

--- src/main/java/com/example/reggie/controller/EmployeeController.java
+++ src/main/java/com/example/reggie/controller/EmployeeController.java
@@ -1,11 +1,13 @@
 package com.example.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.reggie.common.R;
 import com.example.reggie.entity.Employee;
 import com.example.reggie.service.EmployeeService;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -44,7 +46,7 @@ public class EmployeeController {
         }
         //查看账号状态，是否被禁用
         if(emp.getStatus() == 0){
-            return R.error("账号以禁用");
+            return R.error("账号已禁用");
         }
         //登陆成功,将员工id存入session
         request.getSession().setAttribute("employee",emp.getId());
@@ -60,8 +62,8 @@ public class EmployeeController {
 
     /**
      * 新增员工
-     * @param employee
-     * @return
+     * @param employee:
+     * @return :
      */
     @PostMapping
     public R<String> save(HttpServletRequest request, @RequestBody Employee employee){
@@ -76,11 +78,25 @@ public class EmployeeController {
         return R.success("新增员工成功");
     }
 
-
-    @GetMapping
-    public R<String> search(){
-
-        return null;
-
+    /**
+     * 员工信息查询
+     * @param page:
+     * @param pageSize:
+     * @param name:
+     * @return :
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+        //构造分页构造器
+        Page pageInfo = new Page<>(page,pageSize);
+        //构造条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        //添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        //执行查询
+        employeeService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
     }
 }
