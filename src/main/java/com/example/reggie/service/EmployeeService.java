77404e767d1eb1069f4b20e17f3b4f740ffd606b
zwl <1831622781@qq.com>
2023-03-30 14:04:26 +0800

    03301313

--- src/main/java/com/example/reggie/service/EmployeeService.java
+++ src/main/java/com/example/reggie/service/EmployeeService.java
@@ -3,5 +3,6 @@ package com.example.reggie.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.reggie.entity.Employee;
 
+
 public interface EmployeeService extends IService<Employee> {
 }
